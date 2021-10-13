cb5ffcf5fb4e596e32da8348b2582b7da0f5d6a1
村上 尊司 <t-murakami@onestead.local>
2021-10-13 10:52:13 +0900

    implements timer

--- MyTimer/app/src/main/java/com/example/mytimer/MainActivity.java
+++ MyTimer/app/src/main/java/com/example/mytimer/MainActivity.java
@@ -3,12 +3,63 @@ package com.example.mytimer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final Handler mHandler = new Handler(Looper.getMainLooper());
+
+    private Timer mTimer;
+    private TextView mTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTime = (TextView) findViewById(R.id.textView0);
+
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mTimer = new Timer();
+        mTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                final int time = Integer.parseInt(mTime.getText().toString()) - 1;
+
+//                //メインスレッド以外からUI操作（異常終了）
+//                String text = String.valueOf(time);
+//                mTime.setText(text);
+
+                if (time < 0) {
+                    mTimer.cancel();
+                    mTimer = null;
+                } else {
+                    mHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            String text = String.valueOf(time);
+                            mTime.setText(text);
+                        }
+                    });
+                }
+            }
+        }, 1500, 1000);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mTimer.cancel();
+        mTimer = null;
+    }
+
 }
